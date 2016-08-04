286b6f7e17a1b76108f5638ce1fc4238565022c8
Mada Aryakusumah <mada@41studio.com>
2016-08-04 17:18:46 +0700

    hide some property

--- common/models/moment.js
+++ common/models/moment.js
@@ -1,6 +1,10 @@
-var loopback = require('loopback');
-
 module.exports = function(Moment) {
+
+  var isStatic = true;
+  Moment.disableRemoteMethod("findOne", isStatic);
+  Moment.disableRemoteMethod('count', isStatic);
+  Moment.disableRemoteMethod('createChangeStream', isStatic);
+  Moment.disableRemoteMethod("exists", isStatic);
 
   Moment.observe('before save', function event(ctx, next) {
     var loopbackToken = ctx.options.remoteCtx.req.accessToken;
